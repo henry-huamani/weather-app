1a442557a60242edfc762719612f2e4907dd07b1
HENRY HUAMANI <huamanihenry22@gmail.com>
2022-02-05 18:00:45 -0500

    Estilo responsive para el componente Header y SearchBar mejorado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,33 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import styles from "./Header.module.css";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 export default class Header extends React.Component{
     render(){
         let {onSearch} = this.props;
         return(
             <div className={styles.contenedor}>
-                <Link className={styles.home} to="/">
-                    <h1>Home</h1>
-                </Link>
-                <Link className={styles.about} to="/about">
-                    <h1>About</h1>
-                </Link>
+                <NavLink className={styles.home} to="/">
+                    Home
+                </NavLink>
+                <NavLink className={styles.about} to="/about">
+                    About
+                </NavLink>
                 <SearchBar onSearch={onSearch}/>
+                <div className={styles.dropdown}>
+                    <FontAwesomeIcon className={styles.dropbtn} icon={faBars} />
+                    <div className={styles.dropdownContent}>
+                        <NavLink to="/" className={styles.active} >
+                            <h1>Home</h1>
+                        </NavLink>
+                        <NavLink to="/about" className={styles.active}>
+                            <h1>About</h1>
+                        </NavLink>
+                    </div>
+                </div>
             </div>
         )
     }
